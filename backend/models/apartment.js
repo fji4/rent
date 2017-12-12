2535d1c8cb777b45d3d09dcb084f0c4db259d18c
jzhan159 <jzhan159@illinois.edu>
2017-12-12 01:15:58 -0600

    Account api used

---
+++ backend/models/apartment.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var apartmentSchema = mongoose.Schema({
+    location: {type:String,required: true},
+    city: {type:String, required: true},
+    price: {type:String,required: true},
+    assignedOwner: String,
+    gender: String,
+    contactPhone: {type:String, default:""},
+    contactEmail: {type: String, default:""},
+    description: String,
+    datePosted: {type: Date, default: Date.now()},
+    dateStarted: {type: Date, required: true},
+    dateEnd: {type: Date, required: true},
+    completed:{type:Boolean, default: false},
+    img : {type:[String], default: []}
+
+});
+
+module.exports= mongoose.model('Apartment',apartmentSchema);
--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -2,13 +2,39 @@ import React, { Component } from 'react'
 import { Item, Tab, List } from 'semantic-ui-react'
 import { Input, Menu, Form, TextArea, Dropdown } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 require('./Account.scss');
 
 
 class Account extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_id: "5a2dc92d7ad47271b6b7c307",
+            logged_in: false,
+            register: false,
+            cur_user: { local: {}}
+        }
+
+    }
+
+
 
     render() {
+
+        //get the info of current user
+        if (!(this.state.logged_in)) {
+            axios.get('http://localhost:3000/api/users/5a2ce4f2039511da2469e462')
+                .then(function (resp) {
+                    this.setState({ cur_user: resp.data.data, logged_in: true })
+                }.bind(this)
+                )
+        }
+        if (this.state.cur_user)
+            console.log(this.state.cur_user.local.email)
+
+
         const panes = [
             {
                 menuItem: 'Subleasing', render: () => <Tab.Pane attached={false}>Tab 1 Content
@@ -62,7 +88,9 @@ class Account extends Component {
                     <h1> Subleasing </h1>
                     <div> <img src={'http://tc.sinaimg.cn/maxwidth.800/tc.service.weibo.com/static_jinrongbaguanv_com/5886a925e3bd5fc2a3adf8f9a36324c8.png'}
                         alt="fairy" /> </div>
-                    <div> Welcome back! wuzaiyunshang</div>
+                    <div> Welcome back! Default username{//this.state.cur_user.name
+                    }
+                        </div>
                     <div> Search </div>
                     <div>
                         <Link to={{ pathname: "/" }} >
@@ -77,8 +105,12 @@ class Account extends Component {
                         <img src={'http://tc.sinaimg.cn/maxwidth.800/tc.service.weibo.com/static_jinrongbaguanv_com/5886a925e3bd5fc2a3adf8f9a36324c8.png'}
                             alt="fairy" />
                         <ul>
-                            <li>welcome back! wuzaiyunshang</li>
-                            <li>email: jjj@shi.com</li>
+                            <li>Welcome back! Default username{//this.state.cur_user.name
+                            }
+                            </li>
+                            <li>email: {this.state.cur_user.local.email}{//this.state.cur_user.email
+                            }
+                            </li>
                             <li>tel: 88888888</li>
                         </ul>
                     </div>
