468c81a56738073618eb738528da3b0d3603989d
fji4 <fji4@illinois.edu>
2017-12-13 11:34:38 -0600

    asdf

--- index.js
+++ index.js
@@ -135,6 +135,4 @@ app.get('/images/:id', function(req, res) {
 //   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 // });
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000);
