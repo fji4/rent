896f4a64e530f8d4a557a8ac786abe5b50fc936f
fji4 <fji4@illinois.edu>
2018-04-07 08:44:51 -0500

    bootstrap update

--- frontend/src/components/Detail/Detail.jsx
+++ frontend/src/components/Detail/Detail.jsx
@@ -129,7 +129,7 @@ class Detail extends Component {
         return(
             <div className="outdiv">
                 <head>
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"></link>
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
                 </head>
                 <Menu fluid borderless stackable size="massive" className="detailnav">
                     <Menu.Item>
